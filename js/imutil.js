0624b9c322462fb7233671ae0ca573713f9775f9
donglaiw <weiddoonngglai@gmail.com>
2021-11-28 19:24:22 -0500

    js: add printf

--- js/imutil.js
+++ js/imutil.js
@@ -6,3 +6,13 @@ function getUrlParam(name){
 function getUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
+
+function printfd(im_id, num_digit){
+    for(var i=1; i<num_digit; i++){
+        if(im_id<Math.pow(10,i)){
+            im_id = '0'.repeat(num_digit-i)+im_id;
+            return im_id;
+        }
+    }
+    return im_id;
+}
